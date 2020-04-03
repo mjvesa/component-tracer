ebb9bc826c23470727cc9d865a244ccd1f11d6c3
Matti Vesa <mattijvesa@gmail.com>
2020-04-03 23:23:37 +0300

    Remove logging, trim tooltip content

--- preact-tracer.js
+++ preact-tracer.js
@@ -4,14 +4,12 @@ export const installTracer = path => {
   let old = options.vnode;
   options.vnode = vnode => {
     const trace = new Error().stack;
-    console.log(trace);
     let lines = trace.split("\n");
     for (let i = 0; i < lines.length; i++) {
       if (
         (lines[i].indexOf("at h ") > 0 || lines[i].indexOf("h@") > 0) &&
         (lines[i + 1].indexOf("!./") > 0 || lines[i + 1].indexOf("/./") > 0)
       ) {
-        console.log(lines[i + 1]);
         const values = lines[i + 1].split(":");
         const col = values[values.length - 1].replace(/\D/g, "");
         const line = values[values.length - 2].replace(/\D/g, "");
@@ -27,16 +25,14 @@ export const installTracer = path => {
           }
         }
 
-        const url = `vscode://file${path}/${subPath}${file}:${line}:${col}`;
         vnode.attributes = vnode.attributes || {};
         if (!vnode.attributes.title) {
-          vnode.attributes.title = url;
+          vnode.attributes.title = `${subPath}${file}:${line}:${col}`;
           vnode.attributes.onmousedown = event => {
             if (event.shiftKey) {
-              window.open(url, "_blank");
+              window.open(`vscode://file${path}/${subPath}${file}:${line}:${col}`, "_blank");
             }
           };
-          console.log(url);
         }
       }
       if (old) old(vnode);
